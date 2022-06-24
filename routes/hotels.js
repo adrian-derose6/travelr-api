fc750b3d3328c607ef3e353b5296b99f9e3d149c
Adrian DeRose <adrian.derose6@gmail.com>
2022-06-23 19:00:44 -0500

    feat(hotels): create hotels routes

--- routes/hotels.js
+++ routes/hotels.js
@@ -14,8 +14,46 @@ router.post('/', async (req, res) => {
 	}
 });
 //UPDATE
+router.put('/:hotelId', async (req, res) => {
+	try {
+		const updatedHotel = await Hotel.findByIdAndUpdate(
+			req.params.hotelId,
+			{
+				$set: req.body,
+			},
+			{ new: true }
+		);
+		res.status(200).json(updatedHotel);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 //DELETE
+router.delete('/:hotelId', async (req, res) => {
+	try {
+		await Hotel.findByIdAndDelete(req.params.hotelId);
+		res.status(200).json('Hotel has been deleted');
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 //GET
+router.get('/:hotelId', async (req, res) => {
+	try {
+		const hotel = await Hotel.findById(req.params.hotelId);
+		res.status(200).json(hotel);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 //GET ALL
+router.get('/', async (req, res) => {
+	try {
+		const hotels = await Hotel.find();
+		res.status(200).json(hotels);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 
 export default router;
