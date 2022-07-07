ede3367f57325e5fa873a911a751a5ac4d5bd17b
Adrian DeRose <adrian.derose6@gmail.com>
2022-07-06 20:57:03 -0500

    chore: add cors middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
 import authRoutes from './routes/auth.js';
@@ -12,6 +13,7 @@ const app = express();
 dotenv.config();
 
 // middlewares
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(morgan('tiny'));
